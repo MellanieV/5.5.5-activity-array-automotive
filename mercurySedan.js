d74a46cf7c093228f75a62152ece82bd11e366d4
MellanieV <mellanievargas126@gmail.com>
2024-09-17 10:55:13 -0700

    commit -A

--- mercurySedan.js
+++ mercurySedan.js
@@ -5,7 +5,42 @@ const VehicleModule = require("./vehicleBaseClass")
 let v = new VehicleModule.Vehicle("Mercury", "Sedan", "1965", "color", "mileage");
 console.log(v.make)
 
-
+class Car extends VehicleModule {
+    constructor (make, model, year, color) {
+        super (make, model, year, color, mileage);
+        this.maximumPassengers = 5;
+        this.passenger = 0;
+        this.numberOfWheels = 4;
+        this.maximumSpeed = 160;
+        this.fuel = 10;
+        this.schedule = false;
+    }
+}
+loadPassenger (num) {
+    if (this.passenger < this.maxPassengers){
+        if ((num + this.passenger) <= maxPassengers) {
+            this.passenger = num;
+            return this.passenger;
+        } else {
+            console.log (this.model + "" + this.make + "does not have enough space to take all passengers.")
+        }
+    }
+}
+start() {
+    if (this.fuel > 0 ){
+        console.log("engine has started");
+        return this.started = true
+    } else {
+        console.log("no fuel");
+        return this.started = false;
+    }
+}
+checkService() {
+    if (this.milage > 3000){
+        this.scheduleService = true
+        return this.scheduleService;
+    }
+}
 //After you write the derived Car class, you should test it out.
 
 //Note: You can code your derived Car class here or make a file named index.js and do it there.
